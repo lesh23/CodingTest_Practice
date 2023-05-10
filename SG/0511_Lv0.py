a242964ce869d50627a790cbad378aacc827f3e2
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-10 23:24:44 +0900

    Lv0

--- SG/0511_Lv0.py
+++ SG/0511_Lv0.py
@@ -1,13 +1,21 @@
 # 두 수의 곱
-def
 
 # 몫 구하기 
-
+def solution(num1, num2):
+    answer = num1//num2
+    return answer
 
 # 두 수의 차
-
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
 
 # 나머지 구하기
-
+def solution(num1, num2):
+    answer = num1%num2
+    return answer
 
 # 나이 출력
+def solution(age):
+    answer = 2022 - age +1
+    return answer
