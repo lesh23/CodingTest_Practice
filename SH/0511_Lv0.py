b798a327ce6db68be5245e6474ef18a7012a3cf2
lesh23 <lesh9523@gmail.com>
2023-05-11 13:07:48 +0900

    Update 0511_Lv0.py

--- SH/0511_Lv0.py
+++ SH/0511_Lv0.py
@@ -1,13 +1,22 @@
 # 두 수의 곱
-abd
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
 
 # 몫 구하기 
-abcd
+def solution(num1, num2):
+    return num1 // num2
 
 # 두 수의 차
-
+def solution(num1, num2):
+    return num1-num2
 
 # 나머지 구하기
-
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
 
 # 나이 출력
+def solution(age):
+    answer = 2022-age+1
+    return answer
