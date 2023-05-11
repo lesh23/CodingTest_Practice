0ca7cde9839d0dbc8ca0bd4356a7c0ccf1089aea
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-11 23:48:49 +0900

    0511 완료

--- SG/0511_Lv2.py
+++ SG/0511_Lv2.py
@@ -8,10 +8,10 @@ def solution(s):
 # JadenCase 문자열 만들기
 def solution(s):
     answer = ''
-     = s.split(' ')                  #''안에 공백이 있어야 함
-    for i in range(len(l)): 
-        l[i]=l[i].capitalize()       #capitalize: 첫번째 문자만 대문자로 변환
-    answer=' '.join(l)               #join(): 리스트를 문자로 변환
+    a = s.split(' ')                  #''안에 공백이 있어야 함
+    for i in range(len(a)): 
+        a[i]=a[i].capitalize()       #capitalize: 첫번째 문자만 대문자로 변환
+    answer=' '.join(a)               #join(): 리스트를 문자로 변환
     return answer
 
 # 최솟값 만들기
