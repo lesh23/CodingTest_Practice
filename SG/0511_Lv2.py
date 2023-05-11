014558aac978f1b6ea9db7c3fa6ff3220240fe4f
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-11 23:46:23 +0900

    0511 과제 완

--- SG/0511_Lv2.py
+++ SG/0511_Lv2.py
@@ -1,5 +1,25 @@
 # 최댓값과 최솟값
-and
+def solution(s):
+    a=s.split()                     #.split(): 문자열을 리스트로 변환
+    for i in range(len(a)):
+        a[i]=int(a[i])                          
+    return str(min(a)) + " " + str(max(a))
+
 # JadenCase 문자열 만들기
+def solution(s):
+    answer = ''
+     = s.split(' ')                  #''안에 공백이 있어야 함
+    for i in range(len(l)): 
+        l[i]=l[i].capitalize()       #capitalize: 첫번째 문자만 대문자로 변환
+    answer=' '.join(l)               #join(): 리스트를 문자로 변환
+    return answer
 
 # 최솟값 만들기
+def solution(A,B):
+    answer = 0
+    A.sort()                          #sort(): 오름차순 정렬
+    B.sort()
+    n=len(A)
+    for i in range(n):
+        answer += A[i]*B[n-i-1]
+    return answer
