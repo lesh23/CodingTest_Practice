ae97256bfb420a373c8a7dc1e8b8ecfb26164356
lesh23 <lesh9523@gmail.com>
2023-05-12 11:35:47 +0900

    0511_Lv2

--- SH/0511_Lv2.py
+++ SH/0511_Lv2.py
@@ -6,12 +6,19 @@ def solution(s):
 
 # JadenCase 문자열 만들기
 def solution(s):
+    # 전부 다 소문자로 만들고
     l=s.lower().split(' ')
+
     for i in range(len(l)):
+        # 맨 앞글자 대문자로 만들고 그 뒤에 나머지꺼 합체
         if len(l[i]) >= 2:
             l[i] = l[i][0].upper() + l[i][1:] 
+
+         # 연속된 공백이라는 문제 조건 고려 & 한글자짜리 고려
         else :
             l[i] = l[i].upper()
+
+    # 공백으로 join        
     return ' '.join(l)
 
     # 파이썬 내장 함수 이용
