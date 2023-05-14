9c41d22114ba33c24a56da1077add35884436111
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-14 23:16:40 +0900

    완료

--- SG/0515_Lv1.py
+++ SG/0515_Lv1.py
@@ -1,5 +1,30 @@
 # 문자열 내 p와 y의 개수
+def solution(s):
+    cnt_p = 0
+    cnt_y = 0 
+    for i in s:
+        if i=='p' or i=='P':
+            cnt_p += 1
+        if i=='y' or i=='Y':
+            cnt_y += 1
+    if cnt_p == cnt_y:
+        return True
+    else:
+        return False
 
 # 자연수 뒤집어 배열로 만들기
+def solution(n):
+    answer = []
+    l = list(map(int, str(n)))
+    answer = l[::-1]
+    return answer
 
 # 정수 제곱근 판별
+def solution(n):
+    answer = 0 
+    a = n**0.5
+    if a == int(a):
+        answer = (a+1)**2
+    else : 
+        answer = -1
+    return answer 
--- SG/0515_Lv2.py
+++ SG/0515_Lv2.py
@@ -1,5 +1,24 @@
 # 다음 큰 숫자
-
+def solution(n):
+    cnt = bin(n).count('1')
+    for i in range(n+1,1000001):
+        if bin(i).count('1') == cnt:
+            return i
+        
 # 피보나치 수
 
 # 짝지어 제거하기
+def solution(s):
+    stack = []
+    for i in s:
+        if stack ==[]:
+            stack.append(i)
+        else :
+            if i == stack[-1]:
+                stack.pop()
+            else :
+                stack.append(i)
+    if stack != [] :
+        return 0
+    else :
+        return 1
