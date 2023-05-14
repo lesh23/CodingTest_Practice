82b31cfea303aad4dc50b5afb585135a0064e2b4
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-14 23:28:01 +0900

    완료

--- SG/0515_Lv2.py
+++ SG/0515_Lv2.py
@@ -6,6 +6,14 @@ def solution(n):
             return i
         
 # 피보나치 수
+def solution(n):
+    a = 1
+    b = 1
+    if n == 1 or n ==2 :
+        return 1
+    for i in range(1,n) : 
+        a , b = b , b+a
+    return a % 1234567
 
 # 짝지어 제거하기
 def solution(s):
