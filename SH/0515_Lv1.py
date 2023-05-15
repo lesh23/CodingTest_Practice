2d1c4a1ab40bc80de1bc2d9059ffc7ce7be8791a
lesh23 <lesh9523@gmail.com>
2023-05-15 12:55:14 +0900

    0515

--- SH/0515_Lv1.py
+++ SH/0515_Lv1.py
@@ -1,5 +1,17 @@
 # 문자열 내 p와 y의 개수
+def solution(s):
+    s = s.lower()
+    if s.count('p') != s.count('y'):
+        return False
+    return True
 
 # 자연수 뒤집어 배열로 만들기
+def solution(n):
+    return list(reversed(list(map(int, str(n)))))
 
 # 정수 제곱근 판별
+def solution(n):
+    a = n ** (1/2)
+    if int(a)**2 == n:
+        return (a+1)**2
+    return -1
