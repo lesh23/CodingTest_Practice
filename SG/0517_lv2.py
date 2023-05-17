4c27249dc3994d8e05330c8a710e92054c3cbabf
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-17 16:51:47 +0900

    0517

--- SG/0517_lv2.py
+++ SG/0517_lv2.py
@@ -1,5 +1,12 @@
 # 영어 끝말잇기
 
-# 카펫
 
+# 카펫
+def solution(brown, yellow):
+    answer = []
+    for i in range(0,5000):
+        for j in range(0,5000):
+            if i+j == (brown-4)/2 and i*j == yellow:
+                return [j+2,i+2]
+            
 # 구명보트
