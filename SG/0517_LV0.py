6ab28b3b6ab9d0f6eb9f57a1b969ef7e9d617505
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-17 16:42:45 +0900

    0517

--- SG/0517_LV0.py
+++ SG/0517_LV0.py
@@ -1,30 +1,9 @@
 # 숫자 비교하기
-def solution(num1, num2):
-    answer = 0
-    if num1 != num2:
-        answer = -1
-    else :
-        answer = 1
-    return answer
 
 # 소문자로 바꾸기
-def solution(myString):
-    return myString.lower()
 
 # 문자 리스트를 문자열로 변환하기
-def solution(arr):
-    answer = ''
-    for i in arr:
-        answer += str(i)
-    return answer
 
 # 대문자로 바꾸기
-def solution(myString):
-    return myString.upper()
 
 # 공배수
-def solution(number, n, m):
-    if number%n == 0 and number%m==0 :
-        return 1
-    else:
-        return 0
--- SG/0517_Lv2.py
+++ SG/0517_Lv2.py
@@ -1,11 +1,5 @@
 # 영어 끝말잇기
 
 # 카펫
-def solution(brown, yellow):
-    answer = [] 
-    for i in range(0,5000):                             # 범위 다시 생각
-        for j in range(0,5000):
-            if i+j == (brown-4)/2 and i*j == yellow:
-                return [j+2,i+2]                                            
 
 # 구명보트
