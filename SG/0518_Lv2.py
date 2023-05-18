2871d95504ab3bd46f5fc75400eb5ced0e7ee479
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-18 22:31:29 +0900

    다시

--- SG/0518_Lv2.py
+++ SG/0518_Lv2.py
@@ -1,4 +1,17 @@
 # 예상 대진표
+def solution(n,a,b):
+    answer = 0
+    while a != b :
+        if a%2 == 1:
+            a = a//2+1
+        else:
+            a = a//2
+        if b%2==1:
+            b = b//2+1
+        else:
+            b = b//2
+        answer += 1
+    return answer
 
 # 점프와 순간 이동
 
