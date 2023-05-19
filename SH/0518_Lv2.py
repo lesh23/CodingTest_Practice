89e4dd77da807c10105e1b57a6f5e72b55018e8f
lesh23 <lesh9523@gmail.com>
2023-05-19 21:46:42 +0900

    0518

--- SH/0518_Lv2.py
+++ SH/0518_Lv2.py
@@ -1,6 +1,28 @@
 # 예상 대진표
+def solution(n,a,b):
+    a, b = min(a,b), max(a,b)
+    
+    if b-a ==1 and b%2==0:
+        return 1
+    
+    for i in range(2,21):
+        if 2**i == n:
+            i = i
+            break 
+
+    while i > 1:
+        if a <= 2**(i-1) and b > 2**(i-1):
+            return i
+
+        if a > 2**(i-1):
+            a = a - 2**(i-1)
+            b = b - 2**(i-1)
+            
+        i -= 1
+        
 
 # 점프와 순간 이동
+
 
 # N개의 최소공배수
 # 틀림
