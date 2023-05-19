e384969371e1c8e768ec9947b4c98d608c2bdfbc
lesh23 <lesh9523@gmail.com>
2023-05-19 17:01:49 +0900

    0518

--- SH/0518_Lv2.py
+++ SH/0518_Lv2.py
@@ -3,3 +3,19 @@
 # 점프와 순간 이동
 
 # N개의 최소공배수
+# 틀림
+def solution(arr):
+    arr.sort()
+    answer = 1
+    
+    # 최소공배수 구하는 방법으로 접근
+    # [5,6,9] 이런 test case 어떻게 하지
+    for i in range(len(arr)-1):
+        for j in range(i+1,len(arr)):
+            if arr[j] % arr[i] == 0:
+                arr[j] //= arr[i]
+
+    for i in arr:
+        answer *= i    
+    
+    return answer
