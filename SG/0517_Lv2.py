18cbb426c28def2fe4dd0f029c44650bad323ca6
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-21 20:46:42 +0900

    구명보트 다시

--- SG/0517_Lv2.py
+++ SG/0517_Lv2.py
@@ -1,4 +1,12 @@
 # 영어 끝말잇기
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] :
+            return [(i%n)+1, (i//n)+1]
+        elif words[i] in words[:i]:
+            return [(i%n)+1, (i//n)+1]
+    else:
+        return [0,0]
 
 # 카펫
 def solution(brown, yellow):
