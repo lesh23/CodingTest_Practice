3d179f606418a03acab9b27f316735d1c0e56595
SangjiHan <133099077+SangjiHan@users.noreply.github.com>
2023-05-22 00:09:39 +0900

    Update 0519_Lv2.py

--- SG/0519_Lv2.py
+++ SG/0519_Lv2.py
@@ -1,4 +1,9 @@
 # 멀리 뛰기
+def solution(n):
+    answer = [1,2]
+    for i in range(2,n+1):
+        answer.append(answer[i-1]+answer[i-2])
+    return answer[-2]%1234567               # 피보나치..
 
 # 귤 고르기
 
