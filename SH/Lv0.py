c39fc5fca989fd5c2e58550843bd15410104d903
lesh23 <lesh9523@gmail.com>
2023-05-24 23:55:38 +0900

    lv0

--- SH/Lv0.py
+++ SH/Lv0.py
@@ -248,82 +248,285 @@ def solution(numbers, n):
 
 ### 5/24
 # 중앙값 구하기
+def solution(array):
+    array.sort()
+    return array[int(len(array)/2)]
+
 # 순서쌍의 개수
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        if n % i ==0:
+            answer += 1
+    return answer
+
 # 머쓱이보다 키 큰 사람
+def solution(array, height):
+    return sum(1 if x > height else 0 for x in array)
+
 # 중복된 숫자 개수
+def solution(array, n):
+    return array.count(n)
+
 # 최댓값 만들기(1)
+def solution(numbers):
+    numbers.sort(reverse=True)
+    return numbers[0]*numbers[1]
 
-# # 5/25
 # 문자열의 앞의 n글자
+def solution(my_string, n):
+    return my_string[:n]
+
 # 삼각형의 완성조건 (1)
+def solution(sides):
+    a = sides.pop(sides.index(max(sides)))
+    if a < sum(sides):
+        return 1
+    return 2
+
 # 문자열 뒤집기
+def solution(my_string):
+    my_string = list(my_string)
+    return ''.join(my_string[::-1])
+
 # 짝수 홀수 개수
+def solution(num_list):
+    answer = [0,0]
+    
+    for i in num_list:
+        if i%2 == 0:
+            answer[0] += 1
+        else :   
+            answer[1] += 1
+    
+    return answer
+
 # 길이에 따른 연산
+def solution(num_list):
+    if len(num_list) >=11:
+        return sum(num_list)
+    else :
+        answer = 1
+        for i in num_list:
+            answer *= i
+    
+    return answer
 
-# # 5/26
+
+
+### 5/25
 # 옷가게 할인 받기
+def solution(price):
+    if price >= 500000:
+        return int(price*0.8)
+    elif price >= 300000:
+        return int(price*0.9)
+    elif price >= 100000:
+        return int(price*0.95)
+    else :
+        return int(price)
+    
 # 아이스 아메리카노
+def solution(money):
+    return [money//5500, money%5500]
+
 # 첫 번째로 나오는 음수
+def solution(num_list):
+    for i in range(len(num_list)):
+        if num_list[i] <0:
+            return i
+    return -1
+
 # 모음 제거
+def solution(my_string):
+    for i in ['a','e','i','o','u']:
+        my_string = my_string.replace(i,'')
+    return my_string
+
 # 원소들의 곱과 합
+def solution(num_list):
+    mul = 1
+    for i in num_list:
+        mul *= i
+    if mul < sum(num_list) ** 2 :
+        return 1
+    return 0
 
-# # 5/29
 # 정수 찾기
+def solution(num_list, n):
+    if n in num_list:
+        return 1
+    return 0
+
 # 주사위 게임 1
+def solution(a, b):
+    if a*b % 2 ==1:
+        return a**2 + b**2
+    elif (a+b) % 2 ==1:
+        return 2*(a+b)
+    else:
+        return abs(a-b)
+    
 # n 번째 원소까지
+def solution(num_list, n):
+    return num_list[:n]
+
 # 뒤에서 5등 위로
+def solution(num_list):
+    num_list.sort()
+    return num_list[5:]
+
 # 공백으로 구분하기 1
+def solution(my_string):
+    return my_string.split(' ')
 
 
+
+### 5/26
 # rny_string
-# 배열의 유사도
-# 문자 반복 출력하기
-# 자릿수 더하기
-# n 번째 원소부터
+def solution(rny_string):
+    return rny_string.replace('m','rn')
 
+# 배열의 유사도
+def solution(s1, s2):
+    return sum(1 if i in s2 else 0 for i in s1)
+
+# 문자 반복 출력하기
+def solution(my_string, n):
+    answer = ''
+    for i in my_string:
+        answer += i*n
+    return answer
+
+# 자릿수 더하기
+def solution(n):
+    return sum(int(x) for x in str(n))
+
+# n 번째 원소부터
+def solution(num_list, n):
+     return num_list[n-1:]
 
 # 짝수는 싫어요
+def solution(n):
+    return [x for x in range(n+1) if x%2==1]
+
 # 카운트 업
+def solution(start, end):        
+    return [x for x in range(start,end+1)]
+
 # 부분 문자열인지 확인하기
+def solution(my_string, target):
+    if target in my_string:
+        return 1
+    return 0
+
 # 카운트 다운
+def solution(start, end):
+    return [x for x in range(start,end-1,-1)]
+
 # n개 간격의 원소들
+def solution(num_list, n):
+    return num_list[::n]
 
 
+
+### 5/29
 # 문자열 정수의 합
-# 숨어있는 숫자의 덧셈 (1)
-# 문자열 붙여서 출력하기
-# 글자 이어 붙여 문자열 만들기
-# 원하는 문자열 찾기
+def solution(num_str):
+    return sum(list(map(int, num_str)))
 
+# 숨어있는 숫자의 덧셈 (1)
+def solution(my_string):
+    l = [str(x) for x in range(10)]
+    answer = 0 
+    for i in my_string:
+        if i in l:
+            answer += int(i)
+    return answer
+
+# 풀이2 (isdigit 사용)
+def solution(my_string):
+    return sum(int(i) for i in my_string if i.isdigit())
+
+# 풀이3 (정규표현식 사용)
+import re
+def solution(my_string):
+    return sum(int(n) for n in re.sub('[^1-9]', '', my_string))
+
+# 문자열 붙여서 출력하기
+str1, str2 = input().strip().split(' ')
+print(str1+str2)
+
+# 글자 이어 붙여 문자열 만들기
+def solution(my_string, index_list):
+    return ''.join([my_string[i] for i in index_list])
+
+# 원하는 문자열 찾기
+def solution(myString, pat):
+    if pat.lower() in myString.lower():
+        return 1
+    return 0
 
 # 배열 만들기 1
+def solution(n, k):
+    return [x for x in range(1,n+1) if x%k==0]
+
 # 홀짝 구분하기
+a = int(input())
+if a%2 == 0:
+    print(str(a) + ' is even')
+else:
+    print(str(a) + ' is odd')
+
 # 조건에 맞게 수열 변환하기 1
+def solution(arr):
+    for i in range(len(arr)):
+        if arr[i] >= 50 and arr[i]%2 == 0:
+            arr[i] //= 2
+        elif arr[i]<50 and arr[i]%2 ==1:
+            arr[i] *= 2
+    return arr
+
 # 수 조작하기 1
+def solution(n, control):
+    n += control.count('w')
+    n -= control.count('s')
+    n += control.count('d')*10
+    n -= control.count('a')*10
+    return n
+
 # 뒤에서 5등까지
+def solution(num_list):
+    return sorted(num_list)[:5]
 
 
+
+### 5/30
 # 배열에서 문자열 대소문자 변환하기
 # 문자열안에 문자열
 # 제곱수 판별하기
 # 이어 붙인 수
 # 문자열 바꿔서 찾기
-
-
 # 개미 군단
 # 문자열 바꿔서 찾기
 # 접두사인지 확인하기
 # 더 크게 합치기
 # 마지막 두 원소
+def solution(num_list):
+    if num_list[-1] > num_list[-2]:
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(2*num_list[-1])
+    return num_list
 
 
+
+### 5/31
 # 꼬리 문자열
 # 암호 해독
 # 가위 바위 보
 # 세균 증식
 # 홀짝에 따라 다른 값 반환하기
-
-
 # 대문자와 소문자
 # 주사위의 개수
 # 공백으로 구분하기 2
@@ -331,13 +534,13 @@ def solution(numbers, n):
 # 배열의 원소만큼 추가하기
 
 
+
+### 6/1 
 # 부분 문자열 이어 붙여 문자열 만들기
 # 직각삼각형 출력하기
 # 문자열 정렬하기 (1)
 # 최댓값 만들기 (2)
 # 홀수 vs 짝수
-
-
 # n의 배수 고르기
 # 배열의 길이에 따라 다른 연산하기
 # 접미사인지 확인하기
@@ -345,18 +548,20 @@ def solution(numbers, n):
 # 배열 회전시키기
 
 
+
+### 6/2
 # 인덱스 바꾸기
 # 외계행성의 나이
 # 가장 큰 수 찾기
 # 접미사 배열
 # 피자 나눠 먹기 (2)
-
-
 # 0 떼기
 # 문자열 섞기
 # 369게임
 # 5명씩
 # l로 만들기
+
+
 
 
 # 약수 구하기
