7a9bb849af9ffc1650f64cbd93cc1a4da51b2977
lesh23 <lesh9523@gmail.com>
2023-06-26 14:49:04 +0900

    Update Lv2.py

--- SH/Lv2.py
+++ SH/Lv2.py
@@ -520,6 +520,30 @@ def solution(s):
 
 ### 6/8
 # [1차] 캐시
+from collections import deque
+def solution(cacheSize, cities):
+    stack = deque([])
+    for i in range(len(cities)):
+        cities[i] = cities[i].lower()
+    time = 0
+    if cacheSize == 0:
+        time = len(cities)*5
+    else:
+        for i in cities:
+            if i not in stack:
+                stack.append(i)
+                time += 5
+                if len(stack) > cacheSize:
+                    stack.popleft()
+            else:
+                stack.remove(i)
+                stack.append(i)
+                time += 1
+                if len(stack) > cacheSize:
+                    stack.popleft()
+
+    answer = time
+    return answer
 
 
 
