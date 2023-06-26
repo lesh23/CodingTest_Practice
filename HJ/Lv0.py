9964d545e7e0bee102e590416c2aa46d2205c10e
joonleee <88965328+joonleee@users.noreply.github.com>
2023-06-26 13:52:23 +0900

    lv0 update 0626

--- HJ/Lv0.py
+++ HJ/Lv0.py
@@ -980,3 +980,16 @@ def solution(my_string):
         else:
             answer += int(n)
     return answer
+
+# 1로 만들기
+def solution(num_list):
+    answer = 0
+    for num in num_list:
+        while num>1:
+            if num%2==0:
+                num = num/2
+                answer+=1
+            else:
+                num =(num-1)/2
+                answer+=1
+    return answer
