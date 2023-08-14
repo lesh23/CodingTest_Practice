904fe671ac808217df62c2d942154c246df2c5bb
lesh23 <lesh9523@gmail.com>
2023-08-14 12:20:22 +0900

    Update Lv1.py

--- SH/Lv1.py
+++ SH/Lv1.py
@@ -496,6 +496,17 @@ def solution(a, b):
 
 
 # 추억 점수
+def solution(name, yearning, photo):
+    answer = []
+    for i in photo:
+        s = 0
+        for j in i:
+            if j in name:
+                s += yearning[name.index(j)]
+        answer.append(s)            
+    return answer
+
+    
 
 # 폰켓몬
 
